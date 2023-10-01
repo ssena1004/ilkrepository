0c8dc18e8c5bf7a514240e3c588d1fa81f99b507
ssena1004 <116770273+ssena1004@users.noreply.github.com>
2023-10-01 15:16:14 +0300

    sena yazıyı değişti

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Archi's Academy' Sena Github Kursuma Hoşgeldiniz!</p>
+        <p>Git & Github Kursu w/sena </p>
         <a
           className="App-link"
           href="https://reactjs.org"
